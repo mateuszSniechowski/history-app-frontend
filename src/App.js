725c852268bff2fa3f4003afce9d5d82bae1fa11
Krzysztof69 <sniechowskimateusz4@gmail.com>
2023-10-31 16:34:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Search from './components/search/Search';
+import AddNote from './components/addNote/AddNote';
+import AllNotes from './components/allNotes/AllNotes';
+import Update from './components/update/Update';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<AllNotes />}/>
+          <Route path="/search" element={<Search />} />
+          <Route path="/add" element={<AddNote />}/>
+          <Route path='/update/:id' element={<Update />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
